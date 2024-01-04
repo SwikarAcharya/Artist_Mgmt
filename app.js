558fdaa3c88011fe9f94bd3e8b4481ec6f4a0c0c
SwikarAcharya <acharyaswikar25@gmail.com>
2024-01-04 20:55:24 +0545

    sample route created, if this endpoint is hit than a message is sent as a JSON object

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.APP_PORT || 5000;
+
+app.get('/api', (req, res) => {
+    res.status(200).send({
+      success: 1,
+      message: 'Welcome to the API',
+    });
+  
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
