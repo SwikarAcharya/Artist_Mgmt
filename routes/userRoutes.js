c2715d836f83e58612b8299b9d647cef0c2772ab
SwikarAcharya <acharyaswikar25@gmail.com>
2024-01-10 00:54:17 +0545

    JWT validation removed for new user creation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
+router.post('/', UserController.createUser);
 const authenticateToken = require('../auth/validateJWTmiddlewear');
 router.use(authenticateToken);
 
 router.get('/', UserController.listUsers);
-router.post('/', UserController.createUser);
+// commented for now and moved this to line 5 as we wont be having the JWT token if we were to register a new user
+// router.post('/', UserController.createUser);
 router.put('/:userId', UserController.updateUser);
 router.delete('/:userId', UserController.deleteUser);
 
